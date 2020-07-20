3bbce314c959f7edc2bb2f5d5e15148e75821741
Mo Young <>
2020-07-20 22:44:13 +0800

    LeetCode 94: 二叉树的中序遍历 +1

--- 94_binary_tree_inorder_traversal.py
+++ 94_binary_tree_inorder_traversal.py
@@ -2,12 +2,38 @@
 leetcode: 94
 '''
 
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+'''
+给定一个二叉树，返回它的中序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [1,3,2]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/binary-tree-inorder-traversal
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+## Definition for a binary tree node.
+## class TreeNode:
+##     def __init__(self, x):
+##         self.val = x
+##         self.left = None
+##         self.right = None
+#
+#class Solution:
+#    def inorderTraversal(self, root: TreeNode) -> List[int]:
+#        def helper(root):
+#            return helper(root.left) + [root.val] + helper(root.right) if root else []
+#        return helper(root)
 
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
