6388cfaf60a5a802db2281da1136c9bc6c37b149
Mo Young <>
2020-07-21 23:16:47 +0800

    LeetCode 70: 爬楼梯 +2 CORRECT

--- Week_03/70_climbing_stairs.py
+++ Week_03/70_climbing_stairs.py
@@ -30,16 +30,6 @@ leetcode: 70
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
 
-#class Solution:
-#    def climbStairs(self, n: int) -> int:
-#        if (n <= 2): return n
-#        f1, f2, f3 = 1, 2, 3
-#        for i in range(3, n+1):
-#            f3 = f1 + f2
-#            f1 = f2
-#            f2 = f3
-#        return f3
-
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n <= 2: return n
