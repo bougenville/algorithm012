fa4e258850823bc285d13b1dbfd002eae1c8b4a2
Mo Young <>
2020-07-23 22:24:13 +0800

    LeetCode 111: 二叉树的最小深度 +2 CORRECT

--- 111_minimum_depth_of_binary_tree.py
+++ 111_minimum_depth_of_binary_tree.py
@@ -31,6 +31,6 @@ leetcode: 111
 class Solution:
     def minDepth(self, root: TreeNode) -> int:
         if not root: return 0
-        left = self.minDepth(root, left)
-        right = self.minDepth(root, right)
-        return left + right +1 if (left ==0 or right ==0) else min(left, right) + 1
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        return left + right + 1 if (left == 0 or right == 0) else min(left, right) + 1
