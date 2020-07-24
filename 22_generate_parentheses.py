e5ab76947ada8615e18452d786dfc185a3642f70
Young Man <>
2020-07-24 12:58:29 +0800

    LeetCode 22: 括号生成 +1

--- 22_generate_parentheses.py
+++ 22_generate_parentheses.py
@@ -20,6 +20,25 @@ leetcode: 22
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
 
+#class Solution(object):
+#    def generateParenthesis(self, n):
+#        """
+#        :type n: int
+#        :rtype: List[str]
+#        """
+#        res = []
+#        self.dfs(res, n, n, '')
+#        return res
+#
+#    def dfs(self, res, left, right, path):
+#        if left == 0 and right == 0:
+#            res.append(path)
+#            return
+#        if left > 0:
+#            self.dfs(res, left - 1, right, path + '(')
+#        if left < right:
+#            self.dfs(res, left, right - 1, path + ')')
+
 class Solution(object):
     def generateParenthesis(self, n):
         """
