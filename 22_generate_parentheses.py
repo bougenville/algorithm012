a1da1006673aa28732e94dda5ba8812caed5c7c3
Young Man <>
2020-07-24 12:53:54 +0800

    LeetCode 22: 括号生成

--- 22_generate_parentheses.py
+++ 22_generate_parentheses.py
@@ -1,31 +1,40 @@
 '''
 leetcode: 22
+
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且
+有效的括号组合。
+
+示例：
+
+输入：n = 3
+输出：[
+       "((()))",
+       "(()())",
+       "(())()",
+       "()(())",
+       "()()()"
+     ]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/generate-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
 
-from typing import List
-
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
         res = []
-        cur_str = ''
-
-        def dfs(cur_str, left, right):
-            """
-            :param cur_str: 从根结点到叶子结点的路径字符串
-            :param left: 左括号还可以使用的个数
-            :param right: 右括号还可以使用的个数
-            :return:
-            """
-            if left == 0 and right == 0:
-                res.append(cur_str)
-                return
-            if right < left:
-                return
-            if left > 0:
-                dfs(cur_str + '(', left - 1, right)
-            if right > 0:
-                dfs(cur_str + ')', left, right - 1)
-
-        dfs(cur_str, n, n)
+        self.dfs(res, n, n, '')
         return res
+
+    def dfs(self, res, left, right, path):
+        if left == 0 and right == 0:
+            res.append(path)
+            return
+        if left > 0:
+            self.dfs(res, left - 1, right, path + '(')
+        if left < right:
+            self.dfs(res, left, right - 1, path + ')')
