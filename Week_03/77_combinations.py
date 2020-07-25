c4093f58c536419e3c6776b20f4210a9b803be41
Mo Young <>
2020-07-25 16:29:14 +0800

    LeetCode 77: 组合

---
+++ Week_03/77_combinations.py
@@ -0,0 +1,39 @@
+'''
+leetcode: 77
+
+给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例: 输入: n = 4, k = 2
+
+输出:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combinations
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n <= 0 or k <= 0 or k > n: return []
+        res = []
+        self.__dfs(1, k, n, [], res)
+        return res
+
+    def __dfs(self, start, k, n, pre, res):
+        if len(pre) == k:
+            res.append(pre[:])
+            return
+
+        # 注意：这里 i 的上限是归纳得到的
+        for i in range(start, n - (k - len(pre)) + 2):
+            pre.append(i)
+            self.__dfs(i + 1, k, n, pre, res)
+            pre.pop()
