795d6d595ba88436c7ac819d2aa0bcc9daac2855
Mo Young <>
2020-07-26 11:52:32 +0800

    LeetCode 146: LRU缓存机制 +1

--- 146_lru_cache.py
+++ 146_lru_cache.py
@@ -1,22 +1,58 @@
 '''
 leetcode: 146
+
+运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制。
+它应该支持以下操作： 获取数据 get 和 写入数据 put 。
+
+获取数据 get(key) - 如果关键字 (key) 存在于缓存中，
+则获取关键字的值（总是正数），否则返回 -1。
+写入数据 put(key, value) - 如果关键字已经存在，则变更其数据值；
+如果关键字不存在，则插入该组「关键字/值」。当缓存容量达到上限时，
+它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+
+进阶: 你是否可以在 O(1) 时间复杂度内完成这两种操作？
+
+示例: LRUCache cache = new LRUCache( 2 /* 缓存容量 */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // 返回  1
+cache.put(3, 3);    // 该操作会使得关键字 2 作废
+cache.get(2);       // 返回 -1 (未找到)
+cache.put(4, 4);    // 该操作会使得关键字 1 作废
+cache.get(1);       // 返回 -1 (未找到)
+cache.get(3);       // 返回  3
+cache.get(4);       // 返回  4
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/lru-cache
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
-from collections import OrderedDict
-class LRUCache(OrderedDict):
+
+class LRUCache(object):
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.dic = collections.OrderedDict()
+        self.remain = capacity
 
     def get(self, key: int) -> int:
-        if key not in self:
+        if key not in self.dic:
             return -1
-        self.move_to_end(key)
-        return self[key]
+        v = self.dic.pop(key)
+        self.dic[key] = v    # set key as the newest one
+        return v
 
     def put(self, key: int, value: int) -> None:
-        if key in self:
-            self.move_to_end(key)
+        if key in self.dic:
+            self.dic.pop(key)
         else:
-            if len(self) == self.capacity:
-                self.popitem(last = False)
-        self[key] = value
+            if self.remain > 0:
+                self.remain -= 1
+            else:    #self.dic is full
+                self.dic.popitem(last=False)
+        self.dic[key] = value
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
