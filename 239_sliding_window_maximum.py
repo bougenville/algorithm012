53ada40aa0f4b79b084796eb2de90f7a4fa6ed91
Mo Young <>
2020-07-27 20:48:28 +0800

    LeetCode 239: 滑动窗口最大值 +1

--- 239_sliding_window_maximum.py
+++ 239_sliding_window_maximum.py
@@ -1,6 +1,52 @@
 '''
 leetcode: 239
+
+给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。
+你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+
+返回滑动窗口中的最大值。
+
+进阶：你能在线性时间复杂度内解决此题吗？
+
+示例:
+
+输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+输出: [3,3,5,5,6,7]
+解释:
+
+  滑动窗口的位置                最大值
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+ 
+
+提示：
+1. 1 <= nums.length <= 10^5
+2. -10^4 <= nums[i] <= 10^4
+3. 1 <= k <= nums.length
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sliding-window-maximum
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 '''
+
+#class Solution:
+#    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+#        if not nums: return []
+#        window, res = [], []
+#        for i, x in enumerate(nums):
+#            if i >= k and window[0] <= i - k:
+#                window.pop(0)
+#            while window and nums[window[-1]] <= x:
+#                window.pop()
+#            window.append(i)
+#            if i >= k - 1:
+#                res.append(nums[window[0]])
+#        return res
 
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
