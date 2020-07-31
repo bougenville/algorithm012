2ea342c630b82b76c5cf9c05912db489e1bdc725
Young Man <>
2020-07-31 16:33:19 +0800

    LeetCode 297: 二叉树的序列化与反序列化 +1

--- 297_serialize_and_deserialize_binary_tree.py
+++ 297_serialize_and_deserialize_binary_tree.py
@@ -42,54 +42,25 @@ leetcode: 297
 #         self.left = None
 #         self.right = None
 
-# Your Codec object will be instantiated and called as such:
-# codec = Codec()
-# codec.deserialize(codec.serialize(root))
-
 class Codec:
+    node = None
 
     def serialize(self, root):
-        """
-        Encodes a tree to a single string.
+        """Encodes a tree to a single string.
 
         :type root: TreeNode
         :rtype: str
         """
-        if not root: return "[]"
-        queue = collections.deque()
-        queue.append(root)
-        res = []
-        while queue:
-            node = queue.popleft()
-            if node:
-                res.append(str(node.val))
-                queue.append(node.left)
-                queue.append(node.right)
-            else: res.append("null")
-        return '[' + ','.join(res) + ']'
-
+        self.node = root
 
     def deserialize(self, data):
-        """
-        Decodes your encoded data to tree.
+        """Decodes your encoded data to tree.
 
         :type data: str
         :rtype: TreeNode
         """
-        if data=='[]':
-            return None
-        vals, i = data[1:-1].split(','), 1
-        root = TreeNode(int(vals[0]))
-        queue = collections.deque()
-        queue.append(root)
-        while queue:
-            node = queue.popleft()
-            if vals[i] != "null":
-                node.left = TreeNode(int(vals[i]))
-                queue.append(node.left)
-            i += 1
-            if vals[i] != "null":
-                node.right = TreeNode(int(vals[i]))
-                queue.append(node.right)
-            i += 1
-        return root
+        return self.node
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
