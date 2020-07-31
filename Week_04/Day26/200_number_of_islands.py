8d33f7a77b0fb6936e1d5ff1381739dd9afb0c24
Mo Young <>
2020-07-31 08:42:58 +0800

    LeetCode 200: 岛屿数量

---
+++ Week_04/Day26/200_number_of_islands.py
@@ -0,0 +1,59 @@
+'''
+leetcode:200
+
+给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+此外，你可以假设该网格的四条边均被水包围。
+
+示例 1:
+
+输入:
+[
+['1','1','1','1','0'],
+['1','1','0','1','0'],
+['1','1','0','0','0'],
+['0','0','0','0','0']
+]
+输出: 1
+
+示例 2:
+
+输入:
+[
+['1','1','0','0','0'],
+['1','1','0','0','0'],
+['0','0','1','0','0'],
+['0','0','0','1','1']
+]
+输出: 3
+
+解释: 每座岛屿只能由水平和/或竖直方向上相邻的陆地连接而成。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/number-of-islands
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid: return 0
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+
+        return count
+
+    def dfs(self, grid, i, j):
+        if (i < 0 or j < 0 or i >= len(grid)
+                or j >= len(grid[0])
+                or grid[i][j] != '1'):
+            return
+        grid[i][j] = '0'
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
