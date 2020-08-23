52fb811486aa4ab68d82acec5549cd8f44b1363f
Mo Young <>
2020-08-23 17:50:21 +0200

    LeetCode 22: 括号生成

---
+++ Week_07/Day49/22_generate_parentheses.py
@@ -0,0 +1,40 @@
+'''
+leetcode: 22
+
+数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且
+有效的括号组合。
+
+示例：
+
+输入：n = 3
+输出：[
+       "((()))",
+       "(()())",
+       "(())()",
+       "()(())",
+       "()()()"
+     ]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/generate-parentheses
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+        self.dfs(res, n, n, '')
+        return res
+
+    def dfs(self, res, left, right, path):
+        if left == 0 and right == 0:
+            res.append(path)
+            return
+        if left > 0:
+            self.dfs(res, left - 1, right, path + '(')
+        if left < right:
+            self.dfs(res, left, right - 1, path + ')')
