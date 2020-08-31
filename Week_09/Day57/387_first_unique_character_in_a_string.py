bcce1df4eb40bf928b2763ddb316efce4f2cf5fc
Mo Young <>
2020-08-31 03:08:13 +0200

    LeetCode 387: 字符串中的第一个唯一字符

---
+++ Week_09/Day57/387_first_unique_character_in_a_string.py
@@ -0,0 +1,33 @@
+'''
+leetcode: 387
+
+给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+
+示例：
+s = "leetcode"
+返回 0
+
+s = "loveleetcode"
+返回 2
+
+提示：你可以假定该字符串只包含小写字母。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/first-unique-character-in-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        """
+        :type s: str
+        :rtype: int
+        """
+        # build hash map : character and how often it appears
+        c = collections.Counter(s)
+
+        # find the index
+        for k in c:
+            if c[k] == 1:
+                return s.find(k)
+        return -1
