2e46da63454dc6287a8235f4c8a5e569f655f023
Mo Young <>
2020-09-15 22:39:41 +0800

    LeetCode 300: 最长上升子序列

---
+++ Week_09/Day60/300_longest_increasing_subsequence.py
@@ -0,0 +1,36 @@
+'''
+leetcode: 300
+
+给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+示例:
+输入: [10,9,2,5,3,7,101,18]
+输出: 4 
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+
+说明:
+1. 可能会有多种最长上升子序列的组合，你只需要输出对应的长度即可。
+2. 你算法的时间复杂度应该为 O(n^2) 。
+
+进阶: 你能将算法的时间复杂度降低到 O(n log n) 吗?
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+class Solution:
+    def lengthOfLIS(self, nums):
+        tails, res = [0] * len(nums), 0
+        for num in nums:
+            i, j = 0, res
+            while i < j:
+                m = (i + j) // 2
+                if tails[m] < num:
+                    i = m + 1
+                else:
+                    j = m
+            tails[i] = num
+            if j == res:
+                res += 1
+        return res
